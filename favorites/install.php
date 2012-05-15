68045fc18984230e2c93d1d76df3b2eddda9e0fb
AmmoniteNetowrks <chris@ammonitenetworks.com>
2012-05-15 16:24:59 +0000

    site and installs

---
+++ favorites/install.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @package		Favorites
+* @copyright	Copyright (C) 2009 DT Design Inc. All rights reserved.
+* @license		GNU GPLv2 <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>
+* @link 		http://www.dioscouri.com
+*/
+
+/** ensure this file is being included by a parent file */
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+$thisextension = strtolower( "com_favorites" );
+$thisextensionname = substr ( $thisextension, 4 );
+
+
+include JPATH_SITE . '/libraries/dioscouri/component/install.php';
+?>
